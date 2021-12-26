016f258f21c9414d6b2fb6df2d12482854af17b7
hyereen <reenh@naver.com>
2021-12-26 22:58:24 +0900

    solve 4-3

--- algorithm/16637.py
+++ algorithm/16637.py
@@ -13,7 +13,6 @@ for i in range(n):
             res = res - int(sen[i+1])
         elif sen[i] == '*':
             res = res * int(sen[i+1])
-            print(res, int(sen[i+1]))
     if i == del_idx:
         continue
     
