02b52db8e0750393baefbafda498b3cc87e15dcc
hyereen <reenh@naver.com>
2022-03-18 19:01:30 +0900

    solution(bfs) for 1303(전쟁)

--- algorithm/BOJ/1303.py
+++ algorithm/BOJ/1303.py
@@ -9,48 +9,52 @@ dy = [0, 1, 0, -1]
 
 
 def bfs(x, y, cate):
+    cnt = 0
     queue = deque()
 
-    visited[x][y] = cate
+    visited[x][y] = 1
     queue.append((x, y))
 
     while queue:
         cx, cy = queue.popleft()
+        cnt +=1
 
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
-
-            if nx < 0 or nx < 0 or nx >= n or ny >= m:
+            if nx < 0 or ny < 0 or nx >= m or ny >= n: # 조건, nx, ny가 m,n과의 맵핑 주의
                 continue
-            if visited[nx][ny] == 0 and graph[nx][ny] == 'W':
-                visited[nx][ny] = cate
+            if visited[nx][ny] == 0 and graph[nx][ny] == cate:
+                visited[nx][ny] = 1
                 queue.append((nx, ny))
 
+    return cnt
 
+# 그래프 만드는거 주의!!
 for i in range(m):
-    graph.append([])
-    visited.append([])
-    temp = input()
-    for j in temp:
-        graph[i].append(j)
-        visited[i].append(0)
+    temp = list(input())
+    graph.append(temp)
+    visited.append([0]*n)
 
 w_temp = 0
 w_squared = 0
-cate = 0
+
+b_temp = 0
+b_squared = 0
+
 for a in range(m):
     for b in range(n):
         if visited[a][b] == 0 and graph[a][b] == 'W':
-            cate = cate + 1
-            bfs(a, b, cate)
-            w_temp = max(max(visited))
+            w_temp = bfs(a, b, 'W')
             w_squared = w_squared + w_temp * w_temp
             w_temp = 0
+        elif visited[a][b] == 0 and graph[a][b] == 'B':
+            b_temp = bfs(a, b, 'B')
+            b_squared = b_squared + b_temp * b_temp
+            b_temp = 0
 
 
 
-print(visited)
-print(w_squared)
-print(w_temp)
+
+print(w_squared, b_squared)
 
