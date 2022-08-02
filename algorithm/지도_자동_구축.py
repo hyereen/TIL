ad2e231c35eeb9b90e548a3c70d08e99ba08e4cd
hyereen <reenh@naver.com>
2022-08-02 23:14:43 +0900

    solution(dp) for 지도 자동 구축

---
+++ algorithm/지도_자동_구축.py
@@ -0,0 +1,14 @@
+# https://softeer.ai/practice/info.do?idx=1&eid=413
+
+import sys
+
+N = int(sys.stdin.readline())
+dp = [0] * 16 # N의 최댓값은 15니까 0단계 ~ 15단계 총 16개 칸 생성
+dp[0] = 2 # Start 지점의 한변의 점의 개수 = 2
+
+# N번째 단계의 한 변의 점의 개수 = (N-1번째 점의 개수 + N-1번째 점의 개수-1)
+for i in range(1,N+1):
+    dp[i] = dp[i-1] + (dp[i-1] -1)
+
+# print(dp) -> 각 단계에서 한 변의 점의 개수
+print(dp[N]**2) # 총 점의 개수 = 한 변의 점 개수의 제곱
